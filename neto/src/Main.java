1e5d6a610763f1e256f8e49318d6ce0ac56e6233
ivanAnapa <vanya-cherniy@mail.ru>
2022-04-20 22:10:52 +0300

    init dir to git

--- neto/src/Main.java
+++ neto/src/Main.java
@@ -13,6 +13,7 @@ public class Main {
         System.out.println((potatoes / eaters) + " картофелин(а)");
         System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ка) специй");
+
     }
 
 }
